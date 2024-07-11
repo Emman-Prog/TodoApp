ab1cbc3924f42bda71f463a10104cf23c345463a
Emmanuel Dilag <emmanueldilag@gmail.com>
2024-07-11 23:59:17 +0800

    first commit

---
+++ src/components/AddTodo.tsx
@@ -0,0 +1,47 @@
+import React, { useEffect, useRef, useState } from 'react'
+import { toast } from 'react-hot-toast'
+import { useTodo } from '../context'
+import { Input } from './Input'
+
+export const AddTodo = () => {
+  const [input, setInput] = useState<string>('');
+  const inputRef = useRef<HTMLInputElement>(null);
+  const { addTodo } = useTodo()
+
+  useEffect(() => {
+    if (inputRef.current) {
+      inputRef.current.focus()
+    }
+  }, [])
+
+  const handleSubmission = (e: React.FormEvent) => {
+    e.preventDefault()
+    if (input.trim() !== '') {
+      addTodo(input)
+      setInput('')
+      toast.success('Todo added successfully!')
+    } else {
+      toast.error('Todo field cannot be empty!')
+    }
+  }
+
+  return (
+    <form onSubmit={handleSubmission}>
+      <div className="flex item-center w-full max-w-lg gap-2 p-5 m-auto">
+        <Input
+          ref={inputRef}
+          value={input}
+          type="text"
+          onChange={e => setInput(e.target.value)}
+          placeholder="start typing..."
+        />
+        <button
+          type="submit"
+          className="px-5 py-2 text-sm font-normal text-blue-300 bg-blue-900 border-2 border-blue-900 active:scale-95 rounded-xl"
+        >
+          Submit
+        </button>
+      </div>
+    </form>
+  )
+}
